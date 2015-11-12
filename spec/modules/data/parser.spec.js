b62004ecc79d6a3f6b79a7cebb4268b1075f751f
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2015-11-11 21:53:44 -0800

    Added parser tests

---
+++ spec/modules/data/parser.spec.js
@@ -0,0 +1,54 @@
+/*global beforeEach, describe, expect, it, require*/
+describe('Seer test cases:', function() {
+	var data,
+		mocks = {
+			seer: {
+				predict: noop
+			},
+			strainer: {
+				filter: noop
+			}
+		};
+
+	beforeEach(function () {
+		mockery.registerMock('data/seer', mocks.seer);
+		mockery.registerMock('data/strainer', mocks.strainer);
+
+		data = {
+			query: {
+				ids: '1234',
+				metrics: [ 'totalEvents' ]
+			},
+			rows: [
+				[ 'event', '20151105', '09', '43242' ]
+			]
+		}
+	});
+
+	afterEach(function () {
+		mockery.deregisterMock('data/seer');
+		mockery.deregisterMock('data/strainer');
+	});
+
+	it('Use seer and strainer to parse data', function() {
+		var parser = require('data/parser');
+		spyOn(mocks.seer, 'predict');
+		spyOn(mocks.strainer, 'filter').and.returnValue([]);
+
+		parser.parse(data);
+
+		expect(mocks.seer.predict).toHaveBeenCalled();
+		expect(mocks.strainer.filter).toHaveBeenCalled();
+	});
+
+	it('Parse GA date to date object', function() {
+		var parser = require('data/parser'),
+			result;
+		spyOn(mocks.seer, 'predict');
+		spyOn(mocks.strainer, 'filter').and.returnValue(data.rows);
+
+		result = parser.parse(data);
+
+		expect(result.data.real[0].date.toISOString()).toEqual('2015-11-05T17:00:00.000Z');
+	});
+});
