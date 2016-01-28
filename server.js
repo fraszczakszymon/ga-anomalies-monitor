4b4aee8111386948a0f9223cf4fedf31f25ed159
Szymon Fraszczak <fraszczak.szymon+github@gmail.com>
2016-01-28 23:04:15 +0100

    Rename file with routes

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ require('app-module-path').addPath(__dirname + '/modules');
 require('app-module-path').addPath(__dirname + '/controllers');
 var app,
 	config = require('./config/config.json'),
+	controllers = require('controllers'),
 	cors = require('cors'),
 	database = require('storage/database'),
 	express = require('express'),
-	logger = require('utils/logger'),
-	routes = require('controllers/routes');
+	logger = require('utils/logger');
 
 database.init();
 
 app = express();
 app.use(cors());
-app.use(routes);
+app.use(controllers);
 
 app.listen(config.server.port, function () {
 	logger.info('Example app listening at http://localhost:' + config.server.port);
